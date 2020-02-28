b672d822a02af291a2c8e5ada4d06a5d1e376877
Anton Semenov <buzuluk56@yandex.ru>
2020-02-28 20:55:12 +0500

    исправленно

--- test5.php
+++ test5.php
@@ -3,14 +3,13 @@
     $auto["toyota"]=array("model"=>"camry","speed"=>"150","doors"=>"4","year"=>"2018");
     $auto["opel"]=array("model"=>"astra","speed"=>"130","doors"=>"3","year"=>"2017");
 
-    foreach ($auto as $key=>$value )
+ foreach ($auto as $key=>$value )
     {
         echo $key ."<br>";
 
-        foreach ($auto[$key] as $new_key=>$new_value)
-        {
-            echo $new_value ." ";
-        }
+        $autoValue=implode(",",$value);
+        echo $autoValue;
         echo "<br><br>";
     }
+
 ?>
